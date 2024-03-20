248e47a3c159d84177fdb39d8ff5b598b8fc1e64
yankasantosTW <yanka.santos@thoughtworks.com>
2024-03-20 19:56:38 -0300

    updating with the code to force the changes

--- git_config_user_and_email.sh
+++ git_config_user_and_email.sh
@@ -7,3 +7,7 @@ git config user.email yourEmail@email.com
 #If you want to change the author of the commit
 git commit --amend --reset-author
 #Note: Will open the vim prompt to you change the last commit
+
+#Run the comand to force the changes
+git push --force-with-lease
+#Note: You can see the commit tab on github the changes was applyed
