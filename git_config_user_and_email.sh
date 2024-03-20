fcfc2cf60368b8fad20116f421ba9367bdf49e13
yankasantosTW <yanka.santos@thoughtworks.com>
2024-03-20 19:51:26 -0300

    comands to make the setup for your username and email on repo

---
+++ git_config_user_and_email.sh
@@ -0,0 +1,9 @@
+#Set the username
+git config user.name yourUsername
+
+#Set the email
+git config user.email yourEmail@email.com
+
+#If you want to change the author of the commit
+git commit --amend --reset-author
+#Note: Will open the vim prompt to you change the last commit
